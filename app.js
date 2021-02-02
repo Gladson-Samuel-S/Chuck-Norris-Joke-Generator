692eb19dfe1a844bc6901cebafb409aa62d0d4c4
Gladson Samuel S <74543240+Gladson-Samuel-S@users.noreply.github.com>
2021-02-02 23:43:12 +0530

    Chuck jokes with dark/light theme toggler

---
+++ app.js
@@ -0,0 +1,53 @@
+//dark theme event listener
+const button = document.getElementById('toggler');
+
+button.addEventListener('click',toggleLoader);
+
+function toggleLoader(e){
+  document.body.classList.toggle('dark');
+
+  button.classList.toggle('dark');
+  
+  if(button.classList == 'dark'){
+    button.innerText = 'Light';
+  }else{
+    button.innerText = 'dark';
+
+  }
+}
+
+
+//get jokes button event listener
+document.querySelector('.get-jokes').addEventListener('click',getJokes);
+
+function getJokes(e){
+  const number = document.querySelector('input[type="number"]').value;
+
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET',`http://api.icndb.com/jokes/random/${number}`,true)
+
+  xhr.onload = function(){
+    if(this.status === 200){
+      const response = JSON.parse(this.responseText);
+      console.log(response);
+
+      let output = '';
+
+      if(response.type === 'success'){
+        response.value.forEach(function(joke){
+          output += `<li>${joke.joke}</li>`
+        });
+
+      }else{
+        output += '<li>Something went wrong</li>'
+      }
+
+      document.querySelector('.jokes').innerHTML = output;
+    }
+  }
+
+  xhr.send();
+
+  e.preventDefault();
+}
